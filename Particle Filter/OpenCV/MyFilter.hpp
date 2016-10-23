c09cb7e0b5f4f103c4ff47e74f13a1a415538aa5
wwwrrry <Hsin@HsindeMacBook-Pro.local>
2016-10-23 19:52:31 -0400

    define parameters

--- Particle Filter/OpenCV/MyFilter.hpp
+++ Particle Filter/OpenCV/MyFilter.hpp
@@ -28,6 +28,10 @@ class MyFilter {
 public:
     // parameters
     int m_NumParticles = 10000; // number fo particles
+    float m_WallThres = 0.6; // the threshold for considering as a wall
+    float m_LaserRange = 400; // max laser range
+    float m_nCheck = 10; // number of points along a laser line
+    float m_Resolution = 10.0; // convert from log coordinate to map/image coordinate
     
     // openCV
     cv::Mat m_OrigImg; // original image
